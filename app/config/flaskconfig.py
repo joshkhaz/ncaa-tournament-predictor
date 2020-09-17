d867a9db065bb0c333844481df90ec2e6a3cc442
Josh Khazanov <joshuakhazanov2020@u.northwestern.edu>
2020-09-16 22:03:24 -0500

    Removed unnecessary lines

--- app/config/flaskconfig.py
+++ app/config/flaskconfig.py
@@ -16,15 +16,3 @@ MYSQL_PORT = '<insert credentials here>'
 MYSQL_DATABASE = '<insert credentials here>'
 MYSQL_CONNECTION = 'mysql+pymysql'
 SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".format(MYSQL_CONNECTION, MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE)
-
-
-
-'''SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-if SQLALCHEMY_DATABASE_URI is not None:
-    pass
-elif DB_HOST is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
-else:
-    SQLALCHEMY_DATABASE_URI = '{dialect}://{user}:{pw}@{host}:{port}/{db}'.format(dialect=DB_DIALECT, user=DB_USER,
-                                                                                  pw=DB_PW, host=DB_HOST, port=DB_PORT,
-                                                                                  db=DATABASE)'''
